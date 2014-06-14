b4a4b2c2278c54384234675306ff50ad052fdf72
Matt Warren <matt.warren@gmail.com>
2014-06-14 11:07:55 -0600

    Initial Commit

---
+++ Debug/Debug.h
@@ -0,0 +1,19 @@
+//
+//  Debug.h
+//  Debug
+//
+//  Created by Matt Warren on 2014-06-14.
+//  Copyright (c) 2014 Halotis. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Debug.
+FOUNDATION_EXPORT double DebugVersionNumber;
+
+//! Project version string for Debug.
+FOUNDATION_EXPORT const unsigned char DebugVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Debug/PublicHeader.h>
+
+
